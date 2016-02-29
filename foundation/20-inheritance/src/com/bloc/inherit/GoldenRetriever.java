7a36ca29fd5e61b511bd0d93285a790bf72467fd
Yan Komizorov <Yan_Komizorov@cable.comcast.com>
2016-02-28 19:08:49 -0500

    Assignment 20 completed

--- foundation/20-inheritance/src/com/bloc/inherit/GoldenRetriever.java
+++ foundation/20-inheritance/src/com/bloc/inherit/GoldenRetriever.java
@@ -7,3 +7,19 @@ package com.bloc.inherit;
  *	Golden Retrievers shrink to a smaller size after
  *	playing only 3 times.
 /************************************************/
+
+class GoldenRetriever extends Dog{
+
+	void play() {
+		setWeight(getWeight() - WEIGHT_LOST_FROM_FEEDING);
+		if (getWeight() < MINIMUM_WEIGHT) {
+			setWeight(MINIMUM_WEIGHT);
+		}
+		// Pre-increment play counter
+		if (++mPlayCounter == 3) {
+			changeSize(false);
+			mPlayCounter = 0;
+		}
+	}
+
+}
