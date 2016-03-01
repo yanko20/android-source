8ab565b67431906e796aaced5d04375adbe82565
Yan Komizorov <Yan_Komizorov@cable.comcast.com>
2016-03-01 14:59:26 -0500

    Assignment 25 completed

--- foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
+++ foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
@@ -17,25 +17,12 @@ public class Main extends Object {
 	 	 *	Replace the operations below by employing polymorphism
 		/************************************************/
 
-		dog.feed();
-		dog.wash();
-		dog.exercise();
-
-		cat.feed();
-		cat.wash();
-		cat.exercise();
-
-		bird.feed();
-		bird.wash();
-		bird.exercise();
-
-		snake.feed();
-		snake.wash();
-		snake.exercise();
-
-		tarantula.feed();
-		tarantula.wash();
-		tarantula.exercise();
+		Pet[] pets = {dog, cat, bird, snake, tarantula};
+		for(int i = 0; i < pets.length; i++){
+			pets[i].feed();
+			pets[i].wash();
+			pets[i].exercise();
+		}
 
 		/************************************************
 	 	 *	ASSIGNMENT:
